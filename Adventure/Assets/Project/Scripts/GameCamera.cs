9e6635307ab7f2afa4f940fe44dc81169dc53de2
StrangeToHell <41899820+StrangeToHell@users.noreply.github.com>
2018-12-10 15:42:23 +0100

    g4.1

---
+++ Adventure/Assets/Project/Scripts/GameCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour {
+
+
+    public GameObject target;
+    public Vector3 offset;
+    public float focusSpeed = 1f;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * focusSpeed);
+    
+        
+	}
+}
