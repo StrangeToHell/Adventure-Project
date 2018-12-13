ddf86dcd88d52e01030046f5459a35a43b3aa42c
StrangeToHell <41899820+StrangeToHell@users.noreply.github.com>
2018-12-13 13:44:08 +0100

    g4.4

---
+++ Adventure/Assets/Project/Scripts/Menu/menuSceneManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuSceneManager : MonoBehaviour {
+
+	public void OnStart ()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
